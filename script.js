0e41841282957f1b8a3ffa13bfbcf1fbbe440056
Edward <edpantzar@gmail.com>
2018-05-17 10:24:27 -0500

    Added random coloring option

--- script.js
+++ script.js
@@ -3,6 +3,22 @@ document.addEventListener('DOMContentLoaded', function() {
 
     let odd = true;
 
+    const randomColoring = function(square) {
+        let hexidecimal = '#';
+        let letters = ['A', 'B', 'C', 'D', 'E', 'F'];
+        for (let i=0; i<6; i++) {
+            let addDigit = Math.floor(Math.random() * 2);
+            if (addDigit === 1) {
+                hexidecimal += Math.floor(Math.random() * 10);
+            }
+            else {
+                hexidecimal += letters[Math.floor(Math.random() * 6)];
+            }
+        }
+        square.style.cssText += ` background-color: ${hexidecimal};`;
+        return square;
+    }
+
     const blackAndRedColoring = function(square) {
         if (odd === true) {
             square.style.cssText += " background-color: black;";
@@ -15,16 +31,22 @@ document.addEventListener('DOMContentLoaded', function() {
         return square;
     }
 
-    const generateSquare = function() {
+    const generateSquare = function(randomColors) {
         let square = document.createElement('div');
         square.style.cssText = "width: 11.1%; float: left; padding-bottom: 11.1%;";
-        square = blackAndRedColoring(square);
+        if (randomColors === true) {
+            square = randomColoring(square);
+        }
+        else {
+            square = blackAndRedColoring(square);
+        }
         let newSquare = document.body.appendChild(square);
+        console.log(square.style.cssText);
         return newSquare;
     }
 
     for (let i=0; i<90; i++) {
-        generateSquare();
+        generateSquare(true);
     }
 
 });
