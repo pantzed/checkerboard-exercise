727aec0bb23c2d0b981c314594de17a1d31acf7b
Edward <edpantzar@gmail.com>
2018-05-17 09:46:12 -0500

    Added black/red square generator

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-// Your JS goes here
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    let odd = true;
+
+    const blackAndRedColoring = function(square) {
+        if (odd === true) {
+            square.style.cssText += " background-color: black;";
+            odd = false;
+        }
+        else {
+            square.style.cssText += " background-color: red;";
+            odd = true;
+        }
+        return square;
+    }
+
+    const generateSquare = function() {
+        let square = document.createElement('div');
+        square.style.cssText = "width: 11.1%; float: left; padding-bottom: 11.1%;";
+        square = blackAndRedColoring(square);
+        let newSquare = document.body.appendChild(square);
+        return newSquare;
+    }
+
+    for (let i=0; i<90; i++) {
+        generateSquare();
+    }
+
+});
